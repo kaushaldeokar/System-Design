7f654aa907d1b0b99449f20127f7e0914356a75d
Kaushal <kaushal.deokar@gmail.com>
2025-04-04 00:46:27 +0530

    added factory pattern

---
+++ FactoryPattern/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+import java.util.Map;
+
+interface Car{
+
+    default String drive(){
+        return "drive";
+    }
+
+}
+
+class Bmw implements Car{
+
+    @Override
+    public String drive(){
+        return "bmw drive";
+    }
+}
+
+class Tesla implements Car{
+    @Override
+    public String drive(){
+        return "tesla drive";
+    }
+}
+
+
+class Factory{
+
+    public static HashMap<String,Car> carFactory = new HashMap<>();
+
+    static {
+        carFactory.put("bmw",new Bmw());
+        carFactory.put("tesla",new Tesla());
+
+    }
+}
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Factory.carFactory.get("tesla").drive());
+    }
+}
